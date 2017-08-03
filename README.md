60a5b4988a7cbbce35b0ba78197f62d392b45935
qihaiyang <784311112@qq.com>
2017-08-03 11:03:40 +0800

    @EnableTransactionManagement事务处理

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # springboot 和 Mybatis 极简xml配置
 用mybatis-generator自动生成bean和dao
 使用mapper.xml的方式进行对应
+@EnableTransactionManagement事务处理
--- src/main/java/com/hine/SpringBootGirlStudyApplication.java
+++ src/main/java/com/hine/SpringBootGirlStudyApplication.java
@@ -1,11 +1,12 @@
 package com.hine;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 /*@MapperScan("com.hine.mapper")*/
+@EnableTransactionManagement
 public class SpringBootGirlStudyApplication {
 
 	public static void main(String[] args) {
