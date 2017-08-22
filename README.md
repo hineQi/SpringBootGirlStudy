3466c366f31cd0be2cdb33556f6057aae4f79b7e
qihaiyang <784311112@qq.com>
2017-08-22 14:45:54 +0800

    springsecurity权限控制添加

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 用mybatis-generator自动生成bean和dao
 使用mapper.xml的方式进行对应
 @EnableTransactionManagement事务处理
+
+branch2 集成swagger2 生成api文档
+访问http://localhost:8080/swagger-ui.html
--- src/main/java/com/hine/controller/GirlsController.java
+++ src/main/java/com/hine/controller/GirlsController.java
@@ -3,8 +3,12 @@ package com.hine.controller;
 import com.hine.entity.Girl;
 import com.hine.mapper.GirlMapper;
 import com.hine.service.GirlService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -28,6 +32,7 @@ public class GirlsController {
      * 查询所有女生列表
      * @return
      */
+    @ApiOperation(value = "获取所有女生列表", notes = "")
     @GetMapping(value = "/girls")
     public List<Girl> girlList(){
         return girlRepository.findAll();
@@ -38,6 +43,8 @@ public class GirlsController {
      * @param girl
      * @return
      */
+    @ApiOperation(value = "创建女生", notes = "根据Girl对象创建女生")
+    @ApiImplicitParam(name = "girl", value = "女生详细实体girl", required = true, dataType = "Girl")
     @PostMapping(value = "/girls")
     public Girl girlAdd(Girl girl){
         return girlRepository.save(girl);
@@ -48,6 +55,8 @@ public class GirlsController {
      * @param id
      * @return
      */
+    @ApiOperation(value = "获取女生详细信息", notes = "根据url的id来获取女生详细信息")
+    @ApiImplicitParam(name = "id", value = "女生ID", paramType="path", required = true, dataType = "Long")
     @GetMapping(value = "/girls/{id}")
     public Girl girlFindOne(@PathVariable("id") Integer id){
         return girlRepository.findOne(id);
@@ -58,6 +67,11 @@ public class GirlsController {
      * @param girl
      * @return
      */
+    @ApiOperation(value = "更新女生详细信息", notes = "根据url的id来指定更新对象，并根据传过来的girl信息来更新女生详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "女生ID", paramType="path", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "girl", value = "女生详细实体girl", required = true, dataType = "Girl")
+    })
     @PutMapping(value = "/girls/{id}")
     public Girl girlUpdate(Girl girl){
         return girlRepository.save(girl);
@@ -67,7 +81,9 @@ public class GirlsController {
      * 删除某个女生
      * @param id
      */
+    @ApiOperation(value="删除女生", notes="根据url的id来指定删除对象")
     @DeleteMapping(value = "/girls/{id}")
+    @ApiImplicitParam(name = "id", value = "女生ID", paramType="path", required = true, dataType = "Long")
     public void girlDelete(@PathVariable("id") Integer id){
         girlRepository.delete(id);
     }
@@ -77,6 +93,8 @@ public class GirlsController {
      * @param age
      * @return
      */
+    @ApiOperation(value="通过年龄查询女生列表", notes="根据url的age来查询女生列表")
+    @ApiImplicitParam(name = "age", value = "女生Age", paramType="path", required = true, dataType = "int")
     @GetMapping(value = "/girls/age/{age}")
     public List<Girl> girlListByAge(@PathVariable("age") Integer age){
         return girlRepository.findByAge(age);
@@ -85,6 +103,7 @@ public class GirlsController {
     /**
      * 添加两个女孩
      */
+    @ApiIgnore
     @PostMapping(value = "/girls/two")
     public void girlInserTwo(){
         girlService.insertTwoGirl();
